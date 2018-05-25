6cd0159e6a7f01010e028612c93ca4e9fb718d09
DongKienLoi <kienloi10@gmail.com>
2018-05-25 11:59:39 +0700

    Ver 3

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -8,12 +8,12 @@ import { IBook } from '../models/IBook';
 })
 export class BooksComponent implements OnInit {
   books: IBook[] = [
-    {cover: '../assets/asset-0.png', title: 'Xây dựng RESTful API với Nodejs', linkDL: 'google.com'},
-    {cover: '../assets/asset-1.png', title: 'Giáo trình Front-End cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-2.png', title: 'Lập trình C++', linkDL: 'google.com'},
+    {cover: '../assets/asset-0.png', title: 'Angular cơ bản', linkDL: 'google.com'},
+    {cover: '../assets/asset-1.png', title: 'Angular cơ bản', linkDL: 'google.com'},
+    {cover: '../assets/asset-2.png', title: 'Angular cơ bản', linkDL: 'google.com'},
     {cover: '../assets/asset-3.png', title: 'Angular cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-4.png', title: 'Xây dựng RESTful API với Nodejs', linkDL: 'google.com'},
-    {cover: '../assets/asset-5.png', title: 'mongoDB', linkDL: 'google.com'}
+    {cover: '../assets/asset-4.png', title: 'Angular cơ bản', linkDL: 'google.com'},
+    {cover: '../assets/asset-5.png', title: 'Angular cơ bản', linkDL: 'google.com'}
   ];
   stage: boolean;
   list: boolean;
