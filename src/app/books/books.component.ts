af3781746254b6d7aea7ce1892781594d073da56
DongKienLoi <kienloi10@gmail.com>
2018-05-27 19:40:13 +0700

    Config Search Header

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -8,12 +8,12 @@ import { IBook } from '../models/IBook';
 })
 export class BooksComponent implements OnInit {
   books: IBook[] = [
-    {cover: '../assets/asset-0.png', title: 'Angular cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-1.png', title: 'Angular cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-2.png', title: 'Angular cơ bản', linkDL: 'google.com'},
+    {cover: '../assets/asset-0.png', title: 'Xây dựng RESTFUL API với NODEJS', linkDL: 'google.com'},
+    {cover: '../assets/asset-1.png', title: 'Gíao trình FRONT-END cơ bản', linkDL: 'google.com'},
+    {cover: '../assets/asset-2.png', title: 'Lập trình C++', linkDL: 'google.com'},
     {cover: '../assets/asset-3.png', title: 'Angular cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-4.png', title: 'Angular cơ bản', linkDL: 'google.com'},
-    {cover: '../assets/asset-5.png', title: 'Angular cơ bản', linkDL: 'google.com'}
+    {cover: '../assets/asset-4.png', title: 'Xây dựng RESTFUL API với NODEJS', linkDL: 'google.com'},
+    {cover: '../assets/asset-5.png', title: 'mongo DB', linkDL: 'google.com'}
   ];
   stage: boolean;
   list: boolean;
